c0d36056db0074ec4efd2f186576c9b0cef25789
emabrau <144418033+emabrau@users.noreply.github.com>
2023-09-28 20:02:18 +0300

    nezinomas nd skaicius

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ struct Studentas {
     double galutinisBalas;
 };
 
+
 double Mediana(vector<double>& vec) {
     size_t size = vec.size();
     if (size == 0) {
@@ -22,62 +23,68 @@ double Mediana(vector<double>& vec) {
     }
     sort(vec.begin(), vec.end());
     if (size % 2 == 0) {
+       
         return (vec[size / 2 - 1] + vec[size / 2]) / 2.0;
     } else {
+      
         return vec[size / 2];
     }
 }
 
 int main() {
-    int n; 
-    cout << "Iveskite atliktu namu darbu skaiciu: ";
-    cin >> n;
-
     vector<Studentas> studentai;
-
-    int numStudentai;
-    cout << "Iveskite studentu skaiciu: ";
-    cin >> numStudentai;
-
     char skaicMetodas;
+
     cout << "Pasirinkite skaiciavimo metoda (V - vidurkis, M - mediana): ";
     cin >> skaicMetodas;
 
-    for (int i = 0; i < numStudentai; ++i) {
-        Studentas student;
+    while (true) {
+        Studentas studentas;
 
-        cout << "Iveskite studento varda " << i + 1 << ": ";
-        cin >> student.vardas;
-        cout << "Iveskite studento pavarde " << i + 1 << ": ";
-        cin >> student.pavarde;
+        cout << "Iveskite studento varda (spauskite Enter noredami baigti): ";
+        cin.ignore(); 
+        getline(cin, studentas.vardas);
 
-        cout << "Iveskite studento namu darbu ivertinimus " << student.vardas << " " << student.pavarde << ":" << endl;
-        for (int j = 0; j < n; ++j) {
-            double score;
-            cout << "ND " << j + 1 << ": ";
-            cin >> score;
-            student.ndBalai.push_back(score);
+        if (studentas.vardas.empty()) {
+            break; 
         }
 
-        cout << "Iveskite studento egzamino bala " << student.vardas << " " << student.pavarde << ": ";
-        cin >> student.egzaminas;
+        cout << "Iveskite studento pavarde: ";
+        getline(cin, studentas.pavarde);
+
+        double ndBalas;
+        cout << "Iveskite studento namu darbu ivertinimus " << studentas.vardas << " " << studentas.pavarde << " (Spasukite Enter noredami baigti):\n";
+        while (true) {
+            string input;
+            getline(cin, input);
+
+            if (input.empty()) {
+                break; 
+            }
+
+            ndBalas = stod(input);
+            studentas.ndBalai.push_back(ndBalas);
+        }
+
+        cout << "Iveskite studento egzamino bala " << studentas.vardas << " " << studentas.pavarde << ": ";
+        cin >> studentas.egzaminas;
 
         
         if (skaicMetodas == 'V' || skaicMetodas == 'v') {
-            
-            student.galutinisBalas = 0.4 * (accumulate(student.ndBalai.begin(), student.ndBalai.end(), 0.0) / n) + 0.6 * student.egzaminas;
+          
+            studentas.galutinisBalas = 0.4 * (accumulate(studentas.ndBalai.begin(), studentas.ndBalai.end(), 0.0) / studentas.ndBalai.size()) + 0.6 * studentas.egzaminas;
         } else if (skaicMetodas == 'M' || skaicMetodas == 'm') {
-            
-            student.galutinisBalas = 0.4 * Mediana(student.ndBalai) + 0.6 * student.egzaminas;
+           
+            studentas.galutinisBalas = 0.4 * (Mediana(studentas.ndBalai)) + 0.6 * studentas.egzaminas;
         } else {
             cout << "Neteisingas skaiciavimo metodas. Pasirinkite V arba M." << endl;
             return 1;
         }
 
-        studentai.push_back(student);
+        studentai.push_back(studentas);
     }
 
-
+    
     cout << left << setw(15) << "Vardas" << setw(15) << "Pavarde" << setw(15) << "Galutinis balas" << endl;
     cout << string(45, '-') << endl;
 
@@ -88,3 +95,4 @@ int main() {
 
     return 0;
 }
+
