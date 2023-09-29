c9cc221e8e02c7e3392a72ea7ca93ceb7c8c2965
emabrau <144418033+emabrau@users.noreply.github.com>
2023-09-29 14:15:44 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -19,36 +19,27 @@ double Mediana(std::vector<double>& vec) {
     }
 }
 
-void GalutinisBalas(std::vector<Studentas>& studentai, char skaicMetodas) {
+void GalutinisBalas(std::vector<Studentas>& studentai) {
     for (Studentas& studentas : studentai) {
-      
-        if (skaicMetodas == 'V' || skaicMetodas == 'v') {
-            
-            studentas.galutBalas = 0.4 * (std::accumulate(studentas.ndBalai.begin(), studentas.ndBalai.end(), 0.0) / studentas.ndBalai.size()) + 0.6 * studentas.egzaminas;
-        } else if (skaicMetodas == 'M' || skaicMetodas == 'm') {
-            
-            studentas.galutBalas = 0.4 * (Mediana(studentas.ndBalai)) + 0.6 * studentas.egzaminas;
-        }
+        double vidurkis = 0.4 * (std::accumulate(studentas.ndBalai.begin(), studentas.ndBalai.end(), 0.0) / studentas.ndBalai.size()) + 0.6 * studentas.egzaminas;
+        double mediana = 0.4 * Mediana(studentas.ndBalai) + 0.6 * studentas.egzaminas;
+        studentas.galutBalasVid = vidurkis;
+        studentas.galutBalasMed = mediana;
     }
 }
 
 void displayTable(const std::vector<Studentas>& studentai) {
-    
-    std::cout << std::left << std::setw(15) << "Vardas" << std::setw(15) << "Pavarde" << std::setw(15) << "Galutinis balas" << std::endl;
-    std::cout << std::string(45, '-') << std::endl;
+    std::cout << std::left << std::setw(15) << "Vardas" << std::setw(15) << "Pavarde" << std::setw(20) << "Galutinis (Vid.)" << std::setw(20) << "Galutinis (Med.)" << std::endl;
+    std::cout << std::string(70, '-') << std::endl;
 
-   
     for (const Studentas& studentas : studentai) {
-        std::cout << std::left << std::setw(15) << studentas.vardas << std::setw(15) << studentas.pavarde << std::setw(15) << std::fixed << std::setprecision(2) << studentas.galutBalas << std::endl;
+        std::cout << std::left << std::setw(15) << studentas.vardas << std::setw(15) << studentas.pavarde
+                  << std::fixed << std::setprecision(2) << std::setw(20) << studentas.galutBalasVid << std::setw(20) << studentas.galutBalasMed << std::endl;
     }
-
 }
+
+
 void loadDataFromManualInput(std::vector<Studentas>& studentai) {
-    char skaicMetodas;
-
-    std::cout << "Pasirinkite skaiciavimo metoda (V - vidurkis, M - Mediana): ";
-    std::cin >> skaicMetodas;
-
     while (true) {
         Studentas studentas;
 
@@ -76,10 +67,8 @@ void loadDataFromManualInput(std::vector<Studentas>& studentai) {
             ndBalai = std::stod(input);
             studentas.ndBalai.push_back(ndBalai);
         }
-         
 
-        std::cout << "Iveskite studento egzamino rezultata " << 
-       studentas.vardas << " " << studentas.pavarde << ": ";
+        std::cout << "Iveskite studento egzamino rezultata " << studentas.vardas << " " << studentas.pavarde << ": ";
         std::cin >> studentas.egzaminas;
 
         studentai.push_back(studentas);
