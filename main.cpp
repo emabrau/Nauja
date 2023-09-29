92666f015768eb988dbf18696bb68242d13d3713
emabrau <144418033+emabrau@users.noreply.github.com>
2023-09-29 14:29:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include "functions.h"
 #include "data.h"
 
@@ -17,11 +18,18 @@ int main() {
         std::cin >> filename;
         loadData(studentai, filename); 
     } else {
-        std::cerr << "Netinkamas pasirinkimas. Programa baigia darba." << std::endl;
+        std::cerr << "Netinkamas pasirinkimas. Programa baigia darba." << 
+    std::endl;
         return 1;
     }
 
     GalutinisBalas(studentai); 
+
+    std::sort(studentai.begin(), studentai.end(), [](const Studentas& a, 
+    const Studentas& b) {
+        return a.vardas < b.vardas;
+    });
+  
     displayTable(studentai); 
 
     return 0;
