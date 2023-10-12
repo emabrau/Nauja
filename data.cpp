e763d6095ad712c7bc64991643436546ad9cc133
emabrau <144418033+emabrau@users.noreply.github.com>
2023-10-12 22:04:27 +0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -1,133 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <iomanip>
+#include <algorithm>
 #include "data.h"
 #include "functions.h"
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <iomanip>
-#include <stdexcept>
-#include <sstream>
-
-using namespace std;
 
 
-void loadData(std::vector<Studentas>& studentai, const std::string& filename) {
-    try {
-        std::ifstream file(filename);
-        if (!file.is_open()) {
-            throw std::runtime_error("Nepavyko atidaryti failo " + filename);
+void generateAndWriteStudentRecords(const std::string &filename, int size) {
+    std::ofstream outFile(filename);
+    for (int i = size; i >= 1; --i) {
+        Studentas student = generateRandomStudent(i);
+        outFile << student.vardas << " " << student.pavarde;
+        for (double score : student.ndBalai) {
+            outFile << " " << score;
         }
+        outFile << " " << student.egzaminas << " " << student.galutBalas << std::endl;
+    }
+    outFile.close();
+}
 
-        studentai.clear(); 
-        std::string header;
-        std::getline(file, header);
 
-        std::string line;
-        while (std::getline(file, line)) {
-            Studentas studentas;
-          
-            std::istringstream iss(line);
-            iss >> std::setw(15) >> studentas.vardas >> std::setw(15) >> studentas.pavarde;
-            for (int i = 0; i < 15; ++i) {
-                double balas;
-                iss >> std::setw(8) >> balas; 
-                studentas.ndBalai.push_back(balas);
+void processStudentData(const std::string &filename) {
+    std::ifstream inFile(filename);
+    if (!inFile.is_open()) {
+        std::cerr << "Nepavyko atidaryti failo: " << filename << std::endl;
+        return;
+    }
+
+    std::vector<Studentas> students;
+
+    Studentas student;
+    while (inFile >> student.vardas >> student.pavarde) {
+        student.ndBalai.clear();
+        for (int i = 0; i < 10; ++i) {
+            double score;
+            inFile >> score;
+            student.ndBalai.push_back(score);
+        }
+        inFile >> student.egzaminas >> student.galutBalas;
+        students.push_back(student);
+    }
+    inFile.close();
+
+    std::sort(students.begin(), students.end(), compareStudents);
+
+
+    std::ofstream outFileSorted(filename);
+    for (const Studentas &sortedStudent : students) {
+        outFileSorted << sortedStudent.vardas << " " << sortedStudent.pavarde;
+        for (double score : sortedStudent.ndBalai) {
+            outFileSorted << " " << score;
+        }
+        outFileSorted << " " << sortedStudent.egzaminas << " " << sortedStudent.galutBalas << std::endl;
+    }
+    outFileSorted.close();
+
+    std::ifstream inFileSorted(filename);
+    std::ofstream outFileNuskriaustukai("nuskriaustukai_" + filename);
+    std::ofstream outFileKietiakai("kietiakai_" + filename);
+
+    while (inFileSorted >> student.vardas >> student.pavarde) {
+        student.ndBalai.clear();
+        for (int i = 0; i < 10; ++i) {
+            double score;
+            inFileSorted >> score;
+            student.ndBalai.push_back(score);
+        }
+        inFileSorted >> student.egzaminas >> student.galutBalas;
+
+        if (student.galutBalas < 5.0) {
+            outFileNuskriaustukai << student.vardas << " " << student.pavarde;
+            for (double score : student.ndBalai) {
+                outFileNuskriaustukai << " " << score;
             }
-            iss >> studentas.egzaminas;
-
-            studentai.push_back(studentas);
-        }
-
-        file.close();
-    } catch (const std::exception& e) {
-        throw e;
-    }
-}
-
-void loadDataFromManualInput(std::vector<Studentas>& studentai) {
-    try {
-        while (true) {
-            Studentas studentas;
-
-            std::cout << "Iveskite studento varda (noredami baigti spauskite Enter): ";
-            std::cin.ignore(); 
-            getline(std::cin, studentas.vardas);
-
-            if (studentas.vardas.empty()) {
-                break;
+            outFileNuskriaustukai << " " << student.egzaminas << " " << student.galutBalas << std::endl;
+        } else {
+            outFileKietiakai << student.vardas << " " << student.pavarde;
+            for (double score : student.ndBalai) {
+                outFileKietiakai << " " << score;
             }
-
-            std::cout << "Iveskite studento pavarde: ";
-            getline(std::cin, studentas.pavarde);
-
-            double ndBalai;
-            std::cout << "Iveskite studento " << studentas.vardas << " " << studentas.pavarde << " namu darbu balus " << " " << " (Noredami baigti spauskite Enter):\n";
-            while (true) {
-                std::string input;
-                getline(std::cin, input);
-
-                if (input.empty()) {
-                    break; 
-                }
-
-                ndBalai = std::stod(input);
-                studentas.ndBalai.push_back(ndBalai);
-            }
-
-            std::cout << "Iveskite studento " << studentas.vardas << " " << studentas.pavarde << " egzamino rezultata: ";
-            std::cin >> studentas.egzaminas;
-
-            studentai.push_back(studentas);
+            outFileKietiakai << " " << student.egzaminas << " " << student.galutBalas << std::endl;
         }
-    } catch (const std::exception& e) {
-        throw e;
-    }
-}
-
-void generateStudentDataFile(const std::string& filename, int numRecords) {
-    std::ofstream file(filename);
-
-    if (!file.is_open()) {
-        throw std::runtime_error("Failed to open file: " + filename);
     }
 
-    for (int i = 1; i <= numRecords; ++i) {
-        Studentas studentas;
-
-        studentas.vardas = "Vardas" + std::to_string(i);
-        studentas.pavarde = "Pavarde" + std::to_string(i);
-
-        for (int j = 0; j < 15; ++j) {
-            int balas = rand() % 10 + 1;
-            studentas.ndBalai.push_back(static_cast<double>(balas)); 
-        }
-        int egzaminas = rand() % 10 + 1;
-        studentas.egzaminas = static_cast<double>(egzaminas); 
-
-        double sumHomework = 0.0;
-        for (double score : studentas.ndBalai) {
-            sumHomework += score;
-        }
-
-       
-        float vidurkis = 0.4 * (sumHomework / 15) + 0.6 * studentas.egzaminas;
-        studentas.galutBalasVid = vidurkis;
-
-        file << studentas.vardas << " " << studentas.pavarde << " ";
-        for (double score : studentas.ndBalai) {
-            file << score << " ";
-        }
-        file << studentas.egzaminas << " " << studentas.galutBalasVid << "\n";
-    }
-
-    file.close();
+    inFileSorted.close();
+    outFileNuskriaustukai.close();
+    outFileKietiakai.close();
 }
 
 
-
-
-void processStudentData(const std::string& filename) {
-    std::vector<Studentas> studentai;
-    loadData(studentai, filename);
-    GalutinisBalas(studentai);
-    displayTable(studentai);
-}
