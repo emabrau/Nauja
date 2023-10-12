fabbd40e383e0ad8a9f203fb04e11afb5fb9f8d2
emabrau <144418033+emabrau@users.noreply.github.com>
2023-10-12 22:50:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
      std::vector<int> recordSizes = {1000, 10000, 100000, 1000000, 10000000};
 
      for (int size : recordSizes) {
-         std::string filename = "students_" + std::to_string(size) + ".txt";
+         std::string filename = "studentai_" + std::to_string(size) + ".txt";
 
          
          generateAndWriteStudentRecords(filename, size);
@@ -26,6 +26,7 @@
 
      return 0;
  }
+
 
 
 
