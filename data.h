eadd7ea32f064128216a8bce6e2ca214abbda2bb
emabrau <144418033+emabrau@users.noreply.github.com>
2023-10-12 22:04:04 +0300

    Update data.h

--- data.h
+++ data.h
@@ -4,20 +4,17 @@
 #include <string>
 #include <vector>
 
-
 struct Studentas {
     std::string vardas;
     std::string pavarde;
     std::vector<double> ndBalai;
     double egzaminas;
     double galutBalas;
-    double galutBalasMed;
-    double galutBalasVid;
 };
 
-void loadData(std::vector<Studentas>& studentai, const std::string& filename);
-void loadDataFromManualInput(std::vector<Studentas>& studentai);
-void generateStudentDataFile(const std::string& filename, int numRecords);
-void processStudentData(const std::string& filename);
-#endif
 
+void generateAndWriteStudentRecords(const std::string &filename, int size);
+
+void processStudentData(const std::string &filename);
+
+#endif
