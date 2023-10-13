cbf59a884b81246f01014ab0c7e290305f91db57
emabrau <144418033+emabrau@users.noreply.github.com>
2023-10-13 13:48:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,25 +8,22 @@
  #include <algorithm>
  #include "data.h"
 
- int main() {
-     srand(static_cast<unsigned int>(time(nullptr)));
+int main() {
+    srand(static_cast<unsigned int>(time(nullptr)));
 
-     
-     std::vector<int> recordSizes = {1000, 10000, 100000, 1000000, 10000000};
+    std::vector<int> recordSizes = {1000, 10000, 100000, 1000000, 10000000};
+    int repetitions = 5;  
 
-     for (int size : recordSizes) {
-         std::string filename = "studentai_" + std::to_string(size) + ".txt";
+    for (int size : recordSizes) {
+        std::string filename = "studentai_" + std::to_string(size) + ".txt";
 
-         
-         generateAndWriteStudentRecords(filename, size);
+        generateAndWriteStudentRecords(filename, size);
 
-         
-         processStudentData(filename);
-     }
+        processStudentData(filename, size, repetitions);
+    }
 
-     return 0;
- }
-
+    return 0;
+}
 
 
 
