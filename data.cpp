6ab2b3e5fbcc8356f445a8f544df50a474c3677c
emabrau <144418033+emabrau@users.noreply.github.com>
2023-10-13 13:49:19 +0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -8,6 +8,7 @@
 #include "data.h"
 #include "functions.h"
 #include <chrono>
+#include <numeric>
 
 
 
@@ -32,92 +33,87 @@ void generateAndWriteStudentRecords(const std::string &filename, int size) {
 }
 
 
-void processStudentData(const std::string &filename) {
-   auto startTimeReading = std::chrono::high_resolution_clock::now();
+void processStudentData(const std::string &filename, int size, int repetitions) {
+    std::vector<double> readTimes;
+    std::vector<double> sortTimes;
 
-    std::ifstream inFile(filename);
-    if (!inFile.is_open()) {
-        std::cerr << "Nepavyko atidaryti failo: " << filename << std::endl;
-        return;
-    }
+    for (int rep = 0; rep < repetitions; ++rep) {
+        auto startTimeReading = std::chrono::high_resolution_clock::now();
 
-    std::vector<Studentas> students;
-
-    Studentas student;
-    while (inFile >> student.vardas >> student.pavarde) {
-        student.ndBalai.clear();
-        for (int i = 0; i < 10; ++i) {
-            double score;
-            inFile >> score;
-            student.ndBalai.push_back(score);
+        std::ifstream inFile(filename);
+        if (!inFile.is_open()) {
+            std::cerr << "Nepavyko atidaryti failo: " << filename << std::endl;
+            return;
         }
-        inFile >> student.egzaminas >> student.galutBalas;
-        students.push_back(student);
-    }
-    inFile.close();
 
-  auto endTimeReading = std::chrono::high_resolution_clock::now();
-  
-  auto durationReading = std::chrono::duration_cast<std::chrono::seconds>(endTimeReading - startTimeReading);
-  
-  std::cout << "Laikas, per kuri nuskaite " << filename << ": " << durationReading.count() << " sekundes" << std::endl;
+        std::vector<Studentas> students;
 
-  
-  
-    std::sort(students.begin(), students.end(), compareStudents);
-
-   auto startTimeSorting = std::chrono::high_resolution_clock::now();
-
-    std::ofstream outFileSorted(filename);
-    for (const Studentas &sortedStudent : students) {
-        outFileSorted << sortedStudent.vardas << " " << sortedStudent.pavarde;
-        for (double score : sortedStudent.ndBalai) {
-            outFileSorted << " " << score;
-        }
-        outFileSorted << " " << sortedStudent.egzaminas << " " << sortedStudent.galutBalas << std::endl;
-    }
-    outFileSorted.close();
-
-  auto endTimeSorting = std::chrono::high_resolution_clock::now();
-
-  auto durationSorting = std::chrono::duration_cast<std::chrono::seconds>(endTimeSorting - startTimeSorting);
-  std::cout << "Laikas, per kuri isrusiavo: " <<  filename << ": " << durationSorting.count() << " sekundes" << std::endl;
-
-   auto startTimeOutput = std::chrono::high_resolution_clock::now();
-  
-    std::ifstream inFileSorted(filename);
-    std::ofstream outFileNuskriaustukai("nuskriaustukai_" + filename);
-    std::ofstream outFileKietiakai("kietiakai_" + filename);
-
-    while (inFileSorted >> student.vardas >> student.pavarde) {
-        student.ndBalai.clear();
-        for (int i = 0; i < 10; ++i) {
-            double score;
-            inFileSorted >> score;
-            student.ndBalai.push_back(score);
-        }
-        inFileSorted >> student.egzaminas >> student.galutBalas;
-
-        if (student.galutBalas < 5.0) {
-            outFileNuskriaustukai << student.vardas << " " << student.pavarde;
-            for (double score : student.ndBalai) {
-                outFileNuskriaustukai << " " << score;
+        Studentas student;
+        while (inFile >> student.vardas >> student.pavarde) {
+            student.ndBalai.clear();
+            for (int i = 0; i < 10; ++i) {
+                double score;
+                inFile >> score;
+                student.ndBalai.push_back(score);
             }
-            outFileNuskriaustukai << " " << student.egzaminas << " " << student.galutBalas << std::endl;
-        } else {
-            outFileKietiakai << student.vardas << " " << student.pavarde;
-            for (double score : student.ndBalai) {
-                outFileKietiakai << " " << score;
-            }
-            outFileKietiakai << " " << student.egzaminas << " " << student.galutBalas << std::endl;
+            inFile >> student.egzaminas >> student.galutBalas;
+            students.push_back(student);
         }
+        inFile.close();
+
+        auto endTimeReading = std::chrono::high_resolution_clock::now();
+
+        auto durationReading = std::chrono::duration_cast<std::chrono::seconds>(endTimeReading - startTimeReading);
+
+        readTimes.push_back(durationReading.count());
+
+        auto startTimeSorting = std::chrono::high_resolution_clock::now();
+        std::sort(students.begin(), students.end(), compareStudents);
+        auto endTimeSorting = std::chrono::high_resolution_clock::now();
+
+        auto durationSorting = std::chrono::duration_cast<std::chrono::seconds>(endTimeSorting - startTimeSorting);
+        sortTimes.push_back(durationSorting.count());
+
+        std::ofstream outFileSorted(filename);
+        for (const Studentas &sortedStudent : students) {
+            outFileSorted << sortedStudent.vardas << " " << sortedStudent.pavarde;
+            for (double score : sortedStudent.ndBalai) {
+                outFileSorted << " " << score;
+            }
+            outFileSorted << " " << sortedStudent.egzaminas << " " << sortedStudent.galutBalas << std::endl;
+        }
+        outFileSorted.close();
+
+       
+      std::string nuskriaustukaiFilename = "nuskriaustukai" + std::to_string(size) + ".txt";
+      std::string kietiakaiFilename = "kietiakai" + std::to_string(size) + ".txt";
+      
+        std::ofstream outFileNuskriaustukai(nuskriaustukaiFilename);
+        std::ofstream outFileKietiakai(kietiakaiFilename);
+
+        for (const Studentas &sortedStudent : students) {
+            if (sortedStudent.galutBalas < 5.0) {
+                outFileNuskriaustukai << sortedStudent.vardas << " " << sortedStudent.pavarde;
+                for (double score : sortedStudent.ndBalai) {
+                    outFileNuskriaustukai << " " << score;
+                }
+                outFileNuskriaustukai << " " << sortedStudent.egzaminas << " " << sortedStudent.galutBalas << std::endl;
+            } else {
+                outFileKietiakai << sortedStudent.vardas << " " << sortedStudent.pavarde;
+                for (double score : sortedStudent.ndBalai) {
+                    outFileKietiakai << " " << score;
+                }
+                outFileKietiakai << " " << sortedStudent.egzaminas << " " << sortedStudent.galutBalas << std::endl;
+            }
+        }
+        outFileNuskriaustukai.close();
+        outFileKietiakai.close();
     }
 
-    inFileSorted.close();
-    outFileNuskriaustukai.close();
-    outFileKietiakai.close();
-   auto endTimeOutput = std::chrono::high_resolution_clock::now();
-  auto durationOutput = std::chrono::duration_cast<std::chrono::seconds>(endTimeOutput - startTimeOutput);
-  std::cout << "Laikas, per kuri isvede isrusiuotus " << filename <<  "duomenis: " << durationOutput.count() << " sekundes" << std::endl;
+    double averageReadTime = std::accumulate(readTimes.begin(), readTimes.end(), 0.0) / repetitions;
+    double averageSortTime = std::accumulate(sortTimes.begin(), sortTimes.end(), 0.0) / repetitions;
+
+    std::cout << "Vidutinis nuskaitymo laikas: " << averageReadTime << " sekundes" << std::endl;
+    std::cout << "Vidutinis rusiavimo laikas: " << averageSortTime << " sekundes" << std::endl;
 }
 
