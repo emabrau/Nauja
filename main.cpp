2ee92a134a2403d1d0720a7f29d4e10990d51a9c
emabrau <144418033+emabrau@users.noreply.github.com>
2023-10-29 19:18:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
- #include <iostream>
+#include <iostream>
  #include <fstream>
  #include <string>
  #include <vector>
@@ -7,19 +7,37 @@
  #include <iomanip>
  #include <algorithm>
  #include "data.h"
+#include <list>
 
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    std::vector<int> recordSizes = {1000, 10000, 100000, 1000000, 10000000};
-    int repetitions = 5;  
+    std::string choice;
+    std::cout << "Pasirinkinte duomenu struktura (v - vektorius, l - listas): ";
+    std::cin >> choice;
 
-    for (int size : recordSizes) {
-        std::string filename = "studentai_" + std::to_string(size) + ".txt";
+    if (choice == "v") {
+        std::vector<int> recordSizes = {1000, 10000, 100000, 1000000, 10000000};
+        int repetitions = 5;
 
-        generateAndWriteStudentRecords(filename, size);
+        for (int size : recordSizes) {
+            std::string filename = "studentai_" + std::to_string(size) + ".txt";
 
-        processStudentData(filename, size, repetitions);
+            generateAndWriteStudentRecordsV(filename, size);
+            processStudentDataV(filename, size, repetitions);
+        }
+    } else if (choice == "l") {
+         std::list<int> recordSizes = {1000, 10000, 100000, 1000000, 10000000};
+        int repetitions = 5;
+
+        for (int size : recordSizes) {
+            std::string filename = "studentai_" + std::to_string(size) + ".txt";
+
+            generateAndWriteStudentRecordsL(filename, size);
+            processStudentDataL(filename, size, repetitions);
+        }
+    } else {
+        std::cout << "Netinkamas pasirinkimas. Prasau pasirinkti 'v' arba 'l'." << std::endl;
     }
 
     return 0;
