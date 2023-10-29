5b63fd30b6eee38adf24a7e40cac89ddf2e97c29
emabrau <144418033+emabrau@users.noreply.github.com>
2023-10-29 21:21:19 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -14,6 +14,8 @@ bool compareStudents(const Studentas &a, const Studentas &b);
 
 void generateAndWriteStudentRecords(const std::string &filename, int size);
 
-
+double Mediana(std::vector<double>& vec);
+void GalutinisBalas(std::vector<Studentas>& studentai);
+void displayTable(const std::vector<Studentas>& studentai);
 
 #endif
