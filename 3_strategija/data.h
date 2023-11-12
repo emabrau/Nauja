b3bce66e8aea58643324a0a7621c44f2ef459492
emabrau <144418033+emabrau@users.noreply.github.com>
2023-11-12 22:44:36 +0200

    Create data.h

---
+++ 3_strategija/data.h
@@ -0,0 +1,29 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+#include <vector>
+
+struct Studentas {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<double> ndBalai;
+    double egzaminas;
+    double galutBalas;
+double galutBalasMed;
+double galutBalasVid;
+};
+
+
+void generateAndWriteStudentRecordsV(const std::string &filename, int size);
+void generateAndWriteStudentRecordsL(const std::string &filename, int size);
+
+void processStudentDataV(const std::string &filename, int size, int repetitions);
+void processStudentDataL(const std::string &filename, int size, int repetitions);
+
+void loadDataFromManualInput(std::vector<Studentas>& studentai);
+void loadData(std::vector<Studentas>& studentai, const std::string& filename);
+void chooseDataInputMethod(std::vector<Studentas>& studentai);
+void generateRandomData(std::vector<Studentas>& studentai, int count);
+
+#endif
