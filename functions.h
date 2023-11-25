694d5236692f4f85d2c1bfaa86c8d4653764c69c
emabrau <144418033+emabrau@users.noreply.github.com>
2023-11-25 16:03:03 +0200

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,19 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+#include <vector>
+#include "data.h"
+
+class StudentFunctions {
+public:
+    static Studentas generateRandomStudent(int studentoNumeris);
+    static bool compareStudents(const Studentas &a, const Studentas &b);
+    static void generateAndWriteStudentRecords(const std::string &filename, int size);
+    static double Mediana(std::vector<double>& vec);
+    static void GalutinisBalas(std::vector<Studentas>& studentai);
+    static void displayTable(const std::vector<Studentas>& studentai);
+    static const int numNamuDarbu; 
+};
+
+#endif
