1471a6ef6c79c148399ad4ccb8d5e1126b405c85
emabrau <144418033+emabrau@users.noreply.github.com>
2023-11-25 16:02:36 +0200

    Create data.h

---
+++ data.h
@@ -0,0 +1,34 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <string>
+#include <vector>
+
+class Studentas {
+public:
+    std::string vardas;
+    std::string pavarde;
+    std::vector<double> ndBalai;
+    double egzaminas;
+    double galutBalas;
+    double galutBalasMed;
+    double galutBalasVid;
+};
+
+class DataManager {
+public:
+    void generateAndWriteStudentRecordsV(const std::string &filename, int size);
+    void generateAndWriteStudentRecordsL(const std::string &filename, int size);
+
+    void processStudentDataV(const std::string &filename, int size, int repetitions);
+    void processStudentDataL(const std::string &filename, int size, int repetitions);
+
+    void loadDataFromManualInput(std::vector<Studentas>& studentai);
+    void loadData(std::vector<Studentas>& studentai, const std::string& filename);
+    void chooseDataInputMethod(std::vector<Studentas>& studentai);
+    void generateRandomData(std::vector<Studentas>& studentai, int count);
+};
+
+#endif
+
+
